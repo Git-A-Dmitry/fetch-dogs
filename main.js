6b680a0f1782467ee7998e5d669e0d693dd2e854
Dmitry A <adcntt@gmail.com>
2022-08-22 02:14:46 +0300

    Creating the List

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello from my JS');
+async function start() {
+  const response = await fetch('https://dog.ceo/api/breeds/list/all');
+  const data = await response.json();
+  // console.log(data);
+  createBreedList(data.message);
+}
+
+start();
+
+function createBreedList(breedList) {
+  document.getElementById('breed').innerHTML = `
+  <select>
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList)
+          .map(function (breed) {
+            return `<option>${breed}</option>`;
+          })
+          .join('')}
+      </select>
+  `;
+}
